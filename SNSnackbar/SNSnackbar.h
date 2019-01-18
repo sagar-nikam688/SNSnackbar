089a9a6cb5f93a7347b3ff85c49718de5d5fe782
sagar.nikam <sagar.nikam@sspl-mac-05.local>
2019-01-18 12:13:11 +0530

    Initial Commit

---
+++ SNSnackbar/SNSnackbar.h
@@ -0,0 +1,19 @@
+//
+//  SNSnackbar.h
+//  SNSnackbar
+//
+//  Created by sagar.nikam on 18/01/19.
+//  Copyright © 2019 sagar.nikam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SNSnackbar.
+FOUNDATION_EXPORT double SNSnackbarVersionNumber;
+
+//! Project version string for SNSnackbar.
+FOUNDATION_EXPORT const unsigned char SNSnackbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNSnackbar/PublicHeader.h>
+
+
